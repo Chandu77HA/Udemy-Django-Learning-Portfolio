a791a8f5d67a4dede31b56d1ba74b40ce20f5bed
Chandu77HA <hachandan02@gmail.com>
2024-04-08 16:56:35 +0530

    Deployment Changes

--- Portfolio/wsgi.py
+++ Portfolio/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
+settings_module = 'Portfolio.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Portfolio.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
